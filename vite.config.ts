1fbdfd7696de93c866d14bfef37d49a5687933e5
zaimounir <fesinn1@gmail.com>
2025-04-30 18:27:30 +0000

    Build prêt pour Netlify

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from "vite"
-import react from "@vitejs/plugin-react-swc"
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
 
 export default defineConfig({
-  base: "./", // ✅ important pour les chemins relatifs sur Netlify
-  plugins: [react()]
-})
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
+});
